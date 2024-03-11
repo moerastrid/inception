4abd96722c3ee73e0afc2648e61825cff72e444d
Astrid Geels <moerastrid@gmail.com>
2024-03-11 17:22:41 +0100

    wrote some script for mariadb as a test

---
+++ another_inception/src/MariaDB/script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+service mysql start;
+
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@`${DOMAIN_NAME}\` IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+mysql -e "ALTER USER 'root'@`${DOMAIN_NAME}\` IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+exec mysqld_safe
--- another_inception/src/WP_PHP/script.sh
+++ another_inception/src/WP_PHP/script.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sleep (10)
+sleep (10);
 
 wp config create --allow-root\
 	--dbname=$MYSQL_DATABASE\
 	--dbuser=$MYSQL_USER\
 	--dbpass=$MYSQL_USER_PASSWORD\
-	--dbhost=db:3306 --path='~/data/webfiles/wordpress'
+	--dbhost=db:3306 --path='~/data/webfiles/wordpress';
 
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm7.3 -F;

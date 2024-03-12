859c93a6a6ed8c49a28b2a9467350e8ab97112df
Astrid Geels <moerastrid@gmail.com>
2024-03-12 12:31:32 +0100

    working on wordpress

--- another_inception/src/WP_PHP/script.sh
+++ another_inception/src/WP_PHP/script.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-sleep (10);
+wp core download
 
-wp config create --allow-root\
-	--dbname=$MYSQL_DATABASE\
-	--dbuser=$MYSQL_USER\
-	--dbpass=$MYSQL_USER_PASSWORD\
-	--dbhost=db:3306 --path='~/data/webfiles/wordpress';
+# wp config create --allow-root\
+# 	--dbname=$MYSQL_DATABASE\
+# 	--dbuser=$MYSQL_USER\
+# 	--dbpass=$MYSQL_USER_PASSWORD\
+# 	--dbhost=db:3306 --path='/data/webfiles/wordpress';
 
-/usr/sbin/php-fpm7.3 -F;
+wp core install
+
+# /usr/sbin/php-fpm7.3 -F;

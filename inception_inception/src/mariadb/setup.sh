f5ee233e8765f39d7a45f570c23ccd182ae78017
Astrid Geels <moerastrid@gmail.com>
2024-03-14 17:49:24 +0100

    another inception inception

---
+++ inception_inception/src/mariadb/setup.sh
@@ -0,0 +1,32 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         ::::::::             #
+#    setup.sh                                           :+:    :+:             #
+#                                                      +:+                     #
+#    By: ageels <ageels@student.codam.nl>             +#+                      #
+#                                                    +#+                       #
+#    Created: 2024/03/14 16:54:40 by ageels        #+#    #+#                  #
+#    Updated: 2024/03/14 16:54:42 by ageels        ########   odam.nl          #
+#                                                                              #
+# **************************************************************************** #
+
+#!/bin/sh
+
+service mysql start;
+
+cat /var/lib/mysql/.setup 2> /dev/null
+
+if [ $? -ne 0 ]; then
+	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
+	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
+	mysql -e "FLUSH PRIVILEGES";
+	mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
+
+	mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
+	mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
+
+	touch /var/lib/mysql/.setup
+fi
+
+exec mysqld --console

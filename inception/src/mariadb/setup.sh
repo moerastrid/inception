6825b17904d39094f2e3af5dd251b71824c7946d
moerastrid@gmail.com <moerastrid@gmail.com>
2024-03-19 20:11:13 +0100

    save before Jeroen touches it

--- inception/src/mariadb/setup.sh
+++ inception/src/mariadb/setup.sh
@@ -6,7 +6,7 @@
 #    By: ageels <ageels@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2024/03/14 16:54:40 by ageels        #+#    #+#                  #
-#    Updated: 2024/03/19 16:16:26 by astrid        ########   odam.nl          #
+#    Updated: 2024/03/19 20:10:24 by astrid        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,8 +21,7 @@ if [ $? -ne 0 ]; then
 	mysql -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$PASS'";
 	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$USER'@'%'";
 	mysql -e "FLUSH PRIVILEGES";
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS';";
-	mysql -e "FLUSH PRIVILEGES";
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS'";
 
 	mysql $MYSQL_DATABASE -u$ROOT_USER -p$ROOT_PASS
 	mysqladmin -u$ROOT_USER -p$ROOT_PASS shutdown

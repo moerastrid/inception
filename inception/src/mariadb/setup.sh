087909ec14012a5d5414774eb0e933ddc3998c35
moerastrid@gmail.com <moerastrid@gmail.com>
2024-03-19 14:08:39 +0100

    I forgot

--- inception/src/mariadb/setup.sh
+++ inception/src/mariadb/setup.sh
@@ -6,7 +6,7 @@
 #    By: ageels <ageels@student.codam.nl>             +#+                      #
 #                                                    +#+                       #
 #    Created: 2024/03/14 16:54:40 by ageels        #+#    #+#                  #
-#    Updated: 2024/03/19 12:29:11 by astrid        ########   odam.nl          #
+#    Updated: 2024/03/19 13:48:21 by astrid        ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,8 @@ if [ $? -ne 0 ]; then
 	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
 	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
 	mysql -e "FLUSH PRIVILEGES";
-	mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';";
+	mysql -e "FLUSH PRIVILEGES";
 
 	mysql $MYSQL_DATABASE -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD
 	mysqladmin -u$MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD shutdown
